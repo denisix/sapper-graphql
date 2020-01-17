df25148a4235c42047138e175f5a7cb39f5e6e28
dev <dev@work>
2020-01-17 14:16:38 +0200

    - clean unneeded deps

--- src/server.js
+++ src/server.js
@@ -3,47 +3,27 @@ import express from 'express'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
 import { createServer } from 'http'
-import { MongoClient } from 'mongodb'
 import { ApolloServer } from 'apollo-server-express'
 
-//import { resolvers, typeDefs, APP_SECRET, jwt } from './__my_schema'
-import { resolvers, typeDefs } from './_schema_test'
-import jwt from 'jsonwebtoken'
-const APP_SECRET = '11'
+import { resolvers, typeDefs } from './_schema'
 		
 const { PORT, NODE_ENV } = process.env,
-	dev = NODE_ENV === 'development',
-	mongoURI = process.env.mongoURI || 'mongodb://localhost:27017',
-	mongoDB = process.env.mongoDB || 'tasker'
+	dev = NODE_ENV === 'development'
 
 const start = async () => {
 	try {
-		const mongo = await MongoClient.connect(`${mongoURI}/${mongoDB}`, { useNewUrlParser: true, useUnifiedTopology: true })
-		const db = mongo.db(mongoDB)
-		const user = db.collection('user')
-		const project = db.collection('project')
-		const sprint = db.collection('sprint')
-		const task = db.collection('task')
-
 		const apollo = new ApolloServer({
 			typeDefs,
 			resolvers,
 			context: async req => {
-				let token = false, uid = false;
+				let token = false
+
 				if (req.connection && req.connection.context && req.connection.context.token) {
 					token = req.connection.context.token
-					const { id } = jwt.verify(token, APP_SECRET)
-					if (id) uid = id
 				}
 
 				return {
-					db: mongo,
 					token,
-					uid,
-					user,
-					project,
-					sprint,
-					task,
 				}
 			},
 			introspection: !!dev,
